358eb80a8fe0da910c5082e0b3f361898c030f76
Sheldon Perkins <csperkins18@gmail.com>
2019-10-15 21:08:03 -0400

    htmlRoutes.js .get routing to survey.html and catch all routing to home.html

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,2 +1,11 @@
 // A GET Route to /survey which should display the survey page
 // A default, catch-all route that leads to home.html which displays the home page
+var path = require("path");
+// Displays survey page
+app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "survey.html"));
+  });
+//   Catch all route that displays home page
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+  });
