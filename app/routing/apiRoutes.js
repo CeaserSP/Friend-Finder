5feeba079e84fe3bc41196613b34be35421679f7
Sheldon Perkins <csperkins18@gmail.com>
2019-10-15 22:15:53 -0400

    setting up express app. TO DO: Run through workbench, Make sure app is listenin.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,15 @@
 // A GET route with the url /api/friends. This will be used to display a JSON of all possible friends
 // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic
+var express = require("express");
+// setup express app
+var app = express();
+var PORT = process.env.PORT || 3306;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
 // Displays all friends
 app.get("/api/friends", function(req, res) {
     return res.json(friends);
@@ -16,3 +26,9 @@ app.post("/api/friends", function(req, res) {
 
   res.json(newFriend);
 });
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
