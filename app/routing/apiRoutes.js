dd9b2fffc3e629dd259792d04919a7eeb35f581f
Sheldon Perkins <csperkins18@gmail.com>
2019-10-18 23:14:38 -0400

    move setup express app to server.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,9 +1,7 @@
 // A GET route with the url /api/friends. This will be used to display a JSON of all possible friends
 // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic
 
-// setup express app
-var app = express();
-var PORT = 3000;
+
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
--- server.js
+++ server.js
@@ -1,3 +1,7 @@
 // require the basic npm packages we've used in class: express and path
 var express = require("express");
 var path = require("path");
+
+// setup express app
+var app = express();
+var PORT = 3000;
