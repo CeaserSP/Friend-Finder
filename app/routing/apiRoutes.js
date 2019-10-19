b5f3615bc012ee1fc2df5ca924ba4281266e5d3a
Sheldon Perkins <csperkins18@gmail.com>
2019-10-19 00:03:32 -0400

    require friends in apiRoutes.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,11 +1,9 @@
 // A GET route with the url /api/friends. This will be used to display a JSON of all possible friends
 // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic
 
+var friends = require("../data/friends");
 
 
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
 
 // Displays all friends
