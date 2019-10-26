30821f873a3e99c8b210198f3ba4525fdc9da8f2
Sheldon Perkins <csperkins18@gmail.com>
2019-10-26 04:08:15 -0400

    modal display

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,6 @@
 // A GET route with the url /api/friends. This will be used to display a JSON of all possible friends
 // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic
-var friends = require("../data/friends");
+var friends = require("../data/friends.js");
 
 module.exports = function (app, path) {
 
@@ -41,6 +41,6 @@ module.exports = function (app, path) {
         bestMatch = friends[friendIndex];
         // best match
         res.json(bestMatch);
-
+        console.log(bestMatch);
     });
 };
